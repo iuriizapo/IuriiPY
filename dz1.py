7436e44e9a0870512e139380b9d339484fbb111f
Iurii <metallinvest67@gmail.com>
2024-09-26 11:40:06 +0300

    Add files via upload

---
+++ dz1.py
@@ -0,0 +1,5 @@
+number_of_completed_homework = 12
+number_of_hours_spent = 1.5
+name = 'Python,'
+time_for_one_hw = number_of_hours_spent/number_of_completed_homework
+print('Курс:',name,' всего задач: ',number_of_completed_homework,', затрачено часов: ',number_of_hours_spent,', среднее время выполнения: ',time_for_one_hw,'часа.')
---
+++ module_1_3.py
@@ -0,0 +1,8 @@
+name='Iurii'
+print('Name:' + name)
+age=57
+print('Age:', age)
+age=age+1
+print('New Age:', age)
+is_student=5>3
+print('Is Student:', is_student)
