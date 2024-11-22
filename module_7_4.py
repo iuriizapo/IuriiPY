467978db085af58e1ae6c08f8780add015c8f3b6
Iurii <metallinvest67@gmail.com>
2024-11-22 16:28:53 +0400

    Add files via upload

---
+++ module_7_4.py
@@ -0,0 +1,17 @@
+team1_num = 5
+team2_num = 6
+print('В команде Мастера кода участников: %s !' % team1_num)
+print('Итого сегодня в командах участников: %s и %s !' % (5, 6))
+
+score_2 = 42
+print('Команда Волшебники данных решила задач: {} !'.format(score_2))
+team1_time = 18015.2
+print('Волшебники данных решили задачи за {} с !'.format(team1_time))
+
+score_1, score_2 = 40, 42
+print(f'Команды решили {score_1} и {score_2} задач.')
+challenge_result = 'Мастера кода'
+print(f'Результат битвы: победа команды {challenge_result}!')
+
+tasks_total = 82
+print(f'Сегодня было решено {tasks_total} задач, в среднем по {int(team1_time / tasks_total)} секунды на задачу!')
