3df9b1ab54cd5fce20ad06fd8aa060f79b37f4d1
Iurii <metallinvest67@gmail.com>
2024-11-24 08:54:00 +0400

    Add files via upload

--- module_7_5.py
+++ module_7_5.py
@@ -2,29 +2,16 @@ import os
 import time
 
 
-directory = os.getcwd()
-print(directory)
-
-for i in os.walk(directory):
-    print(i)
-filepath = os.getcwd()
-print(filepath)
 for dirpath, dirnames, filenames in os.walk("."):
     # перебрать каталоги
     for dirname in dirnames:
         print("Каталог:", os.path.join(dirpath, dirname))
     # перебрать файлы
     for filename in filenames:
-        print("Файл:", os.path.join(dirpath, filename))
-filename = 'module_7_5.py'
-filetime = os.path.getmtime(filename)
-print(filetime)
-filesize = os.stat('module_7_5.py').st_size
-print(filesize)
-filesize = os.path.getsize(filename)
-print(filesize)
-formatted_time = time.strftime("%d.%m.%Y %H:%M", time.localtime(filetime))
-print(formatted_time)
-parent_dir = os.path.dirname(filepath)
-print(parent_dir)
-print(f'Обнаружен файл: {filename}, Путь: {filepath}, Размер: {filesize} байт, Время изменения: {formatted_time}, Родительская директория: {parent_dir}')
+        filepath = os.getcwd()
+        filetime = os.path.getmtime(filename)
+        filesize = os.path.getsize(filename)
+        formatted_time = time.strftime("%d.%m.%Y %H:%M", time.localtime(filetime))
+        parent_dir = os.path.dirname(filepath)
+        print(f'Обнаружен файл: {filename}, Путь: {filepath}, Размер: {filesize} байт, Время изменения:'
+              f' {formatted_time}, Родительская директория: {parent_dir}')
