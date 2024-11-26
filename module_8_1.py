0e05f9463f82ef9e6bfa834535c28c51103a3a4e
Iurii <metallinvest67@gmail.com>
2024-11-26 12:23:26 +0400

    Add files via upload

---
+++ module_8_1.py
@@ -0,0 +1,11 @@
+def add_everything_up(a, b):
+    try:
+        c = a + b
+        return c
+    except:
+        c = f'{a}{b}'
+        return c
+
+print(add_everything_up(123.456, 'строка'))
+print(add_everything_up('яблоко', 4215))
+print(add_everything_up(123.456, 7))
