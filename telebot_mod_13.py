d76d08d25a4f955ea7a7190fbd0d094ef5deb1a2
Iurii <metallinvest67@gmail.com>
2025-01-19 11:12:27 +0400

    Update telebot_mod_13.py

--- telebot_mod_13.py
+++ telebot_mod_13.py
@@ -1,10 +1,23 @@
 from aiogram import Bot, Dispatcher, executor
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
 from aiogram.dispatcher.filters.state import StatesGroup, State
+from aiogram.types import ReplyKeyboardMarkup, KeyboardButton, InlineKeyboardMarkup, InlineKeyboardButton
+
 
 api = ''
 bot = Bot(token=api)
 dp = Dispatcher(bot, storage=MemoryStorage())
+keyboard = ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=True)
+button = KeyboardButton(text= 'Информация')
+keyboard.add(button)
+keyboard_2 = ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=True)
+button_2 = KeyboardButton(text= 'Рассчитать')
+keyboard_2.add(button_2)
+kb = InlineKeyboardMarkup()
+but_1 = InlineKeyboardButton(text='Норма калорий', callback_data='calories')
+but_2 = InlineKeyboardButton(text='Формулы расчёта', callback_data='formulas')
+kb.add(but_1, but_2)
+
 
 
 class UserState(StatesGroup):
@@ -13,10 +26,24 @@ class UserState(StatesGroup):
     growth = State()
     weight = State()
 
-@dp.message_handler(text= 'Калории')
-async def set_age(message):
-    await message.answer("Введите свой возраст(полных лет): ")
+@dp.message_handler(text= 'Рассчитать')
+async def main_menu(message):
+    await message.answer('Выберите опцию:',reply_markup=kb)
+
+@dp.callback_query_handler(text='formulas')
+async def get_formulas(call):
+    await call.message.answer('''
+    для мужчин:
+    10 х вес (кг) + 
+    6,25 x рост (см) – 
+    5 х возраст (г) + 5''')
+    await call.answer()
+
+@dp.callback_query_handler(text='calories')
+async def set_age(call):
+    await call.message.answer("Введите свой возраст(полных лет): ")
     await UserState.age.set()
+    #await call.answer()
 
 @dp.message_handler(state=UserState.age)
 async def set_growth(message, state):
@@ -51,14 +78,14 @@ async def fsm_handler(message, state):
     await message.answer(f'Доставка будет отправлена на {data["first"]}')
     await state.finish()
 
-@dp.message_handler(text=['Urban'])
-async def urban_message(message):
-    print('Urban message')
+@dp.message_handler(text=['Информация'])
+async def info_message(message):
+    await message.answer('Если хочешь узнать свою норму калорий, жми: Рассчитать',reply_markup=keyboard_2)
 
 @dp.message_handler(commands=['start'])
 async def start_message(message):
-    await message.answer('Привет! Я бот помогающий твоему здоровью,'
-                         ' если хочешь узнать свою норму, напиши: Калории.')
+    await message.answer('Привет. Я бот помогающий твоему здоровью!',reply_markup=keyboard)   #
+
 
 
 @dp.message_handler()
@@ -70,4 +97,5 @@ async def all_message(message):
 
 if __name__ == '__main__':
     executor.start_polling(dp, skip_updates=True)
+
 
